8fac2c904a32ecbbf9e84e780ba9b80922b2e0da
Nagender Singh Chauhan <nagendersinghchauhan8690@gmail.com>
2024-06-02 22:22:23 +0530

    Update index.php

--- index.php
+++ index.php
@@ -60,8 +60,8 @@ session_start();
             <button type="submit" class="btn btn-primary" name="submit">Submit</button>
         </form>
     </div>
-    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script> -->
 </body>
 </html>
